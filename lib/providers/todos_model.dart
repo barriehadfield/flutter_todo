f71bcd805ec8860d4c15ca7936e866516482c3f3
Barrie Hadfield <barrie+github@tyger.com>
2019-12-29 03:13:04 +0000

    nxt chapter

---
+++ lib/providers/todos_model.dart
@@ -0,0 +1,30 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import 'package:todos/models/task.dart';
+
+class TodosModel extends ChangeNotifier {
+  final List<Task> _tasks = [];
+
+  UnmodifiableListView<Task> get allTasks => UnmodifiableListView(_tasks);
+
+  UnmodifiableListView<Task> get incompleteTasks =>
+      UnmodifiableListView(_tasks.where((todo) => !todo.completed));
+  UnmodifiableListView<Task> get completedTasks =>
+      UnmodifiableListView(_tasks.where((todo) => todo.completed));
+
+  void addTask(Task task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void toggleTodo(Task task) {
+    final taskIndex = _tasks.indexOf(task);
+    _tasks[taskIndex].toggleCompleted();
+    notifyListeners();
+  }
+
+  void deleteTodo(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
